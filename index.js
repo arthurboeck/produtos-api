201b68417c10f649d67d2d954ad7e61d599750c4
Arthur Boeck <arthurguterres@outlook.com>
2023-08-31 21:50:10 -0300

    remove bodyParser

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import bodyParser from 'body-parser';
 import express from 'express';
 import productController from './src/controller/product-controller.js';
 import swaggerDocument from './swagger-output.json' assert { type: "json" };
@@ -7,7 +6,7 @@ import swaggerUi from 'swagger-ui-express';
 const router = express();
 const port = process.env.PORT || 8080;
 router.use(express.urlencoded({ extended: true }))
-router.use(bodyParser.json());
+router.use(express.json());
 
 const server = router.listen(port, () => console.log(`App listening on port ${port}!`));
 server.keepAliveTimeout = 120 * 1000;
